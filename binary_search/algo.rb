af80742392682c13207a31583f890ebb25f8d7f4
dani <danifromecuador@gmail.com>
2024-01-12 18:59:00 -0500

    binary-search-1

---
+++ binary_search/algo.rb
@@ -0,0 +1,26 @@
+def search(nums, target)
+  min = 0
+  max = nums.size-1
+  mid = (min+max)/2
+
+  def recursion(nums, target, min, mid, max)
+    return min if target == nums[min]
+    return mid if target == nums[mid]
+    return max if target == nums[max]
+    return -1 if (min == mid || mid == max)
+
+    if target < nums[mid]
+      max = mid
+      mid = (min+max)/2
+      return recursion(nums, target, min, mid, max)
+    end
+
+    if target > nums[mid]
+      min = mid
+      mid = (min+max)/2
+      return recursion(nums, target, min, mid, max)
+    end
+  end
+
+  recursion(nums, target, min, mid, max)
+end
