e25c05ad2dd49c62090c6265b9401303913dbc3f
dani <danifromecuador@gmail.com>
2024-01-13 23:22:40 -0500

    First attempt

---
+++ first_bad_version/algo.rb
@@ -0,0 +1,23 @@
+# The is_bad_version API is already defined for you.
+# @param {Integer} version
+# @return {boolean} whether the version is bad
+# def is_bad_version(version):
+
+# @param {Integer} n
+# @return {Integer}
+
+def is_bad_version(n)
+  return true if n == 4
+  return false if n == 3
+end
+
+def first_bad_version(n)
+  return n if is_bad_version(n)
+
+  
+  
+  
+      
+end
+
+p first_bad_version(10)
